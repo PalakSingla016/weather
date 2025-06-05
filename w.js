8594847800d9da26d77e177be599dd7b6703589d
PalakSingla016 <palaknlg160904@gmail.com>
2025-06-05 12:00:03 +0530

    Initial commit

--- w.js
+++ w.js
@@ -1,6 +1,8 @@
 const button = document.getElementById("1");
+const time = document.getElementById("time");
 const input = document.getElementById("input");
-//const city = document.getElementById("place");
+const city = document.getElementById("place");
+const temperature = document.getElementById("temp");
 
 async function getData(city){
     const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=0a1f40912131448fb10101818250406&q=${city}&aqi=yes`)
@@ -12,4 +14,8 @@ button.addEventListener('click' , async ()=>{
     const value = input.value;
     const result = await getData(value);
     console.log(result);
+
+    city.innerText =`${result.location.name}, ${result.location.country}`;
+    time.innerText = result.location.localtime;
+    temperature.innerText = result.current.temp_c;
 });
